b897bea481d5bdcd0ca5627ee1e3a531f74f8a4c
Davide Crestini <davide.crestini94@gmail.com>
2017-09-07 19:09:05 +0100

    Started semantic ui

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment, Button, Icon } from 'semantic-ui-react'
 
 const Home = () => (
-  <div>
-    <h1>Amazing home page</h1>
-  </div>
+  <Container text>
+              <Header
+                as='h1'
+                content='Davide Crestini'
+                inverted
+                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+              />
+              <Header
+                as='h2'
+                content='Do whatever you want when you want to.'
+                inverted
+                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+              />
+              <Button primary size='huge'>
+                Get Started
+                <Icon name='right arrow' />
+              </Button>
+            </Container>
 )
 
 export default Home
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment, Button } from 'semantic-ui-react'
 
-const Navbar = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
+export default class Navbar extends Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      activeItem: 'home'
+    }
+    
+  }
 
-export default Navbar
+  handleItemClick (e, { name }) {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+       <Menu fixed='top' size='large'>
+        <Container>
+          <Menu.Item as={Link} to='/'>Home</Menu.Item>
+          <Menu.Item as={Link} to='/about'>About</Menu.Item>
+          
+        </Container>
+  </Menu>
+    )
+  }
+}

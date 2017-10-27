98581bc17f7c5b7e18abc51c58a7c4ff22595286
Davide <davide.crestini94@gmail.com>
2017-10-27 23:48:45 +0100

    Added terminal screen

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,25 +1,70 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './landing.scss'
 
 class Landing extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      command: 'Dio mostro'
+    }
+  }
+
+  onChange(e,val) {
+    console.log(e, val)
+  }
+
   render(){
+    const { command } = this.state
+
+    const history = [
+      '1994 - Born in Sansepolcro',
+      '1995 - Started supporting Juventus',
+      '2001 - Started using my first computer',
+      '2002 - Joined my first Basketball team',
+      '2006 - italy won the Wolrd Cup!',
+      '2009 - Met my fantastic girlfriend <3',
+      '2010 - Started coding...',
+      '2013 - Graduated at business at technical school',
+      '2016 - Bachelor degree in Computer Science at University of Bologna',
+      '2017 - Move to London',
+      '2017 - Started Working at School Explained'
+    ]
+
+
    return (
     <div className="landing">
-      <div className="container">
-        <div className="header">
-          <span>ubuntu@dcrestini:</span> whoami
-        <div/>
-        <div>
-          <h3 className="title is-1">
-            Hello!<br />I'm Davide Crestini and I'm a software developer.
-          </h3>
-          <h2 className="subtitle is-4 has-text-light">
-            This is my own website, look at what I've been doing, I hope you'll enjoy it!
-          </h2>
+      <div className="container terminal">
+        <div className="terminal-menu">
+          <div className="terminal-buttons btn-close" />
+          <div className="terminal-buttons btn-minimize" />
+          <div className="terminal-buttons btn-zoom" />
+        </div>
+        <div className="terminal-screen">
+          <p className="text">Hello!</p>
+          <p className="command">dcrestini:~$ <span>whoami</span></p>
+          <p className="text">I'm Davide Crestini and I'm a software developer!</p>
+          <p className="command">dcrestini:~$ <span>history</span></p>
+          <p className="text">
+            {_.map(history, x => <span>{x}<br/></span>)}
+          </p>
+          <p className="command">dcrestini:~$ <span>man website</span></p>
+          <p className="text">Please type "enter" to visit my website</p>
+          <p className="command with-cursor">
+            dcrestini:~$
+            <span
+              className="content-editable" contenteditable="true"
+              role="input"
+              tabIndex="0"
+              onChange={(e) => this.onChange(e)}
+            >
+              {command}
+            </span>
+          </p>
         </div>
       </div>
-        </div>
     </div>
     )
   }

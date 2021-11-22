066438b90acb4655713b7ecc98f7d626c319be21
Davide Crestini <davide.crestini@theculturetrip.com>
2021-11-22 01:33:29 +0000

    chore: added netlify functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,33 @@
-import preprocess from 'svelte-preprocess';
-
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+import adapter from "@sveltejs/adapter-netlify";
+import { imagetools } from "vite-imagetools";
+import { mdsvex } from "mdsvex";
+import preprocess from "svelte-preprocess";
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+const config = {
+  extensions: [".svelte", ".md", ".svelte.md"],
+  preprocess: [
+    mdsvex({ extensions: [".svelte.md", ".md", ".svx"] }),
+    preprocess({
+      scss: {
+        prependData: "@import 'src/lib/styles/variables.scss';",
+      },
+    }),
+  ],
+  kit: {
+    adapter: adapter(),
+    files: {
+      hooks: "src/hooks",
+    },
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+    vite: {
+      define: {
+        "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString()),
+      },
+      plugins: [imagetools({ force: true })],
+    },
+  },
 };
 
 export default config;
